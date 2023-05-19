d628db79e953eccfd6446bb87b63d8d68c1fd490
Juan Pablo Gonzalez <jupago14@gmail.com>
2023-05-19 15:30:22 -0500

    Table finished

--- src/components/AddContent/NewCategory.jsx
+++ src/components/AddContent/NewCategory.jsx
@@ -1,17 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../../assets/css/AddElements.module.css";
 import TextField from "@mui/material/TextField";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "../button/Button";
 import { validateTitle } from "../../validators/validateCategory";
 
-const NewCategory = () => {
+const NewCategory = ({ categoryList }) => {
   const [title, setTitle] = useState({ value: "", valid: null });
   const [description, setDescription] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  //Data Table
+  useEffect(() => {
+    categoryList.map((cat) => setMovieList(cat));
+  });
+  //Styling
   const styleReplacement = {
     style: {
       color: "#e5e5e5",
       marginInline: "10px",
     },
   };
+
   return (
     <div className={styles.addNewContent}>
       <form className={styles.form}>
@@ -54,6 +69,46 @@ const NewCategory = () => {
           }}
         />
       </form>
+
+      <TableContainer
+        component={Paper}
+        className={`${styles.input} ${styles.table}`}
+        sx={{ marginBlockStart: "1rem", maxInlineSize: "80%" }}
+      >
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Nombre</TableCell>
+              <TableCell align="right">Descripción</TableCell>
+              <TableCell align="right">Editar</TableCell>
+              <TableCell align="right">Remover</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {movieList.map((data, i) => (
+              <TableRow
+                key={data.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {data.nombre}
+                </TableCell>
+                <TableCell align="right">
+                  {data.description
+                    ? data.description
+                    : "No description available"}
+                </TableCell>
+                <TableCell align="right">
+                  <Button styling="gray" description="Editar"></Button>
+                </TableCell>
+                <TableCell align="right">
+                  <Button styling="gray" description="Remover"></Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
--- src/pages/NewContent.jsx
+++ src/pages/NewContent.jsx
@@ -1,19 +1,33 @@
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { search } from "../api/Api";
 import Card from "../components/AddContent/Card";
 import Header from "../components/header/Header";
 import NewVideo from "../components/AddContent/NewVideo";
 import NewCategory from "../components/AddContent/NewCategory";
 
 const NewContent = ({ path }) => {
+  const [categoryList, setCatList] = useState([]);
   const url = useLocation().pathname;
+  const moviesUrl = "/movies";
   let catTest = <p>Page undefined</p>;
+
+  useEffect(() => {
+    search(moviesUrl, setCatList);
+  }, []);
 
   //Input video
   if (url === "/new/video") {
     catTest = <NewVideo />;
     //Inputs category
   } else if (url === "/new/category") {
-    catTest = <NewCategory />;
+    catTest = (
+      <NewCategory
+        categoryList={
+          categoryList && categoryList.map((data, i) => data.categories)
+        }
+      />
+    );
   }
 
   return (
